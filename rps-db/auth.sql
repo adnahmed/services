6ff0208d38e3baf83f60e54c1ede09fcefe16279
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-05-23 02:22:27 +0500

    We cannot afford to write TESTS at this time

--- rps-db/auth.sql
+++ rps-db/auth.sql
@@ -4,13 +4,11 @@
 begin;
 	-- `pgcrypto` to salt and hash passwords and generate random session tokens
 	-- `citext` case insensitive text type for usernames
-	-- `pgtap` for describing and running tests. *requires installation*
 
 \echo 'Creating extensions'
 
 create extension pgcrypto;
 create extension citext;
-create extension pgtap;
 
 
 
